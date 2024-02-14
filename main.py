f0cbac31a907d3b4d1c2ec88e53fe4558e4986dc
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-14 08:38:52 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# import required libraries
+import cv2
+import numpy as num
+
+# sets the video capture to webcam
+video = cv2.VideoCapture(0)
+
+# only runs if the video opened
+if video.IsOpened() is not None:
+  pass
