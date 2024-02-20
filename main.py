8471b63148c28055c5ff4fc846e5b88460aaaab1
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-20 08:26:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,20 +23,31 @@ while video.isOpened() == True:
     #print(booleanReady)
     #print(frame)
 
+    height, width = frame.shape[:2]
+
+    zerosRectangle = num.zeros((height, width), dtype="uint8")
+
+    topleftx = int(width * 0.175)
+    toplefty = int(height * 0.725)
+    bottomrightx = int(width * 0.85)
+    bottomrighty = int(height * 0.25)
+
+    varList = [topleftx, toplefty, bottomrightx, bottomrighty]
+
     # grayscale
     grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # uses the functions from the other files to optimize, make a rectangle, and crop
     optimized = optimize(grey)
-    rectangled = rectangle(frame)
-    cropped = crop(optimized)
+    rectangled = rectangle(frame, varList)
+    cropped = crop(optimized, zerosRectangle, varList)
 
     # detect the lines
     lines = cv2.HoughLines(cropped, 1, num.pi/180, 200)
 
     # print(lines)
 
-    contoured = contours(optimized, rectangled)
+    contoured = contours(cropped, rectangled)
     # look up how to compare two vectors
 
     # draw the final lines
