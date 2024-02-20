fce7f2c1969e3ad920f909d3a92a0a8158b788a0
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-19 20:03:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,53 @@
+# try cv2.drawContours()
+
 # import required libraries
 import cv2
 import numpy as num
+import math
 
-# sets the video capture to webcam
+# import functions
+from reader import rectangle, crop
+from optimization import optimize
+from lineDraw import drawing, contours
+
+# video capture
 video = cv2.VideoCapture(0)
 
-# only runs if the video opened
-if video.IsOpened() is not None:
-  pass
+print(video.isOpened())
+
+# check if the video is opened
+
+while video.isOpened() == True:
+    booleanReady, frame = video.read()
+
+    #print(booleanReady)
+    #print(frame)
+
+    # grayscale
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # uses the functions from the other files to optimize, make a rectangle, and crop
+    optimized = optimize(grey)
+    rectangled = rectangle(frame)
+    cropped = crop(optimized)
+
+    # detect the lines
+    lines = cv2.HoughLines(cropped, 1, num.pi/180, 200)
+
+    # print(lines)
+
+    contoured = contours(optimized, rectangled)
+    # look up how to compare two vectors
+
+    # draw the final lines
+    # final = drawing(rectangled, lines)
+
+    # print(final)
+
+    cv2.imshow('Frame', contoured)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
