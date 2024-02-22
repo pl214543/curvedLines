f8e043d37177eccf399e97631d58e84640d4b45a
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-22 08:36:51 -0500

    Update lineDraw.py

--- lineDraw.py
+++ lineDraw.py
@@ -2,7 +2,8 @@
 import cv2
 import numpy as num
 
-# import drawing function
+# import distance function
+from distanceDetect import distance
 
 # function for drawing the straight lines
 def drawing(frame, lines):
@@ -22,23 +23,23 @@ def drawing(frame, lines):
             # b stores the value of sin(theta) in b
             b = num.sin(theta)
 
-            # x0 stores the value rcos(theta)
+            # x0 stores the value rCos(theta)
             x0 = a * r
 
-            # y0 stores the value rsin(theta)
+            # y0 stores the value rSin(theta)
             y0 = b * r
 
             # x1 variable for the final line
             x1 = int(x0 + 1000 * (-b))
 
             # y1 variable for the final line
-            y1 = int(y0 + 1000 * (a))
+            y1 = int(y0 + 1000 * a)
 
             # x2 variable for the final line
             x2 = int(x0 - 1000 * (-b))
 
             # y2 variable for the final line
-            y2 = int(y0 - 1000 * (a))
+            y2 = int(y0 - 1000 * a)
 
             # cv2.line draws a line on the frame
             cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
@@ -47,40 +48,41 @@ def drawing(frame, lines):
     return frame
 
 # function for drawing contours
-def contours(frame, rectangled):
+def contours(frame, addRectangle):
 
     # finds the contours and hierarchy (order)
     contours, hierarchy = cv2.findContours(frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    midpointList = []
+    pointsList = []
 
     # draws the contours on the frame
-    # cv2.drawContours(rectangled, contours, -1, (0, 255, 0), 3)
+    # cv2.drawContours(addRectangle, contours, -1, (0, 255, 0), 3)
 
     for contour in contours:
 
-        approx = cv2.approxPolyDP(contour, 0.009 * cv2.arcLength(contour, True), True)
+        approx = cv2.approxPolyDP(contour, 0.001 * cv2.arcLength(contour, True), True)
 
-        cv2.drawContours(rectangled, [approx], 0, (0, 0, 255), 5)
+        cv2.drawContours(addRectangle, [approx], 0, (0, 255, 0), 5)
 
         n = approx.ravel()
         i = 0
 
         for j in n:
-            if (i % 2 == 0):
+            if i % 2 == 0:
                 x = n[i]
                 y = n[i + 1]
 
-                # String containing the co-ordinates.
-                string = str(x) + " " + str(y)
+                if i == 0:
+                    pass
 
-                if (i == 0):
-                    # text on topmost co-ordinate.
-                    cv2.putText(rectangled, "Arrow tip", (x, y),
-                                cv2.FONT_HERSHEY_COMPLEX , 0.5, (255, 0, 0))
                 else:
-                    midpointList.append(string)
+
+                    # appends the point to a list
+                    pointsList.append([x, y])
+
             i = i + 1
 
+        distanceEqual = distance(frame, pointsList)
+
         # returns the final edited frame with contours
-    return rectangled
+    return addRectangle
