cfa91e432a247e7147950e1902d9c78b47425942
Alexey Guydouk <144239447+pl214543@users.noreply.github.com>
2024-02-22 08:35:46 -0500

    Update distanceDetect.py

--- distanceDetect.py
+++ distanceDetect.py
@@ -1,5 +1,18 @@
 import cv2
 import numpy as num
 
-def distance(frame, midpoint):
-    pass
+def distance(frame, pointsList):
+
+    counter = 0
+    
+    if pointsList is not None:
+    
+        for point in pointsList:
+    
+            point1 = point
+    
+            counter += 1
+    
+            point2 = pointsList[counter]
+    
+            # figure out how to calculate the distance between the two
